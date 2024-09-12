738d822525ff67b8984fc182b11219aa4d94e202
Đậu Khoa <khoakomlem@gmail.com>
2024-09-12 23:42:38 +0700

    select model

--- src/lib/useUnlock.ts
+++ src/lib/useUnlock.ts
@@ -1,18 +1,22 @@
+import { MODELS } from "@/components/SelectModel"
 import { create } from "zustand"
 import { immer } from "zustand/middleware/immer"
 
 type State = {
 	unlockMap: Record<string, boolean>
+	modelname: string
 }
 
 type Actions = {
 	unlock(name: string): void
 	lock(name: string): void
+	selectModel(name: string): void
 }
 
 export const useUnlock = create<State & Actions>()(
 	immer((set) => ({
 		unlockMap: {},
+		modelname: MODELS[0],
 		unlock: (name: string) => {
 			set((state) => {
 				state.unlockMap[name] = true
@@ -21,6 +25,11 @@ export const useUnlock = create<State & Actions>()(
 		lock: (name: string) => {
 			set((state) => {
 				state.unlockMap[name] = false
+			})
+		},
+		selectModel(name: string) {
+			set((state) => {
+				state.modelname = name
 			})
 		},
 	}))
